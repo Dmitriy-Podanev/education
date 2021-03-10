c8aa6a98ac1070c6b4e6cf1c063902cce99486f9
PodanevDmitriy <podaneovd@mail.ru>
2021-03-10 19:48:19 +0300

    v0.2

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import React from 'react';
-import io from "socket.io-client";
-
 import {Signin} from "./components/Signin";
-const socket = io();
+
 export const App = () => {
   return (
     <div>
--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './assets/styles/main.css';
-import { App } from './App';
+import {App} from './App';
 
 ReactDOM.render(
   <React.StrictMode>
@@ -9,3 +8,5 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
+
+
